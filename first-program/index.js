0abce97cf32b2626ca7b2129aecaf867422135c3
CodeWithMohaimin <dev.mohaimin@gmail.com>
2023-03-25 12:14:02 +0600

    write typescript dunction

---
+++ first-program/index.js
@@ -0,0 +1,4 @@
+function sum(number1, number2) {
+    console.log(number1 + number2);
+}
+sum(10, 20);
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,1 +1,5 @@
-const a: number = 5;
+function sum(number1: number, number2: number) {
+  console.log(number1 + number2);
+}
+
+sum(10, 20);
