8218b998db06b62723354ca93534b4a9bcd1f9af
CodeWithMohaimin <dev.mohaimin@gmail.com>
2023-03-25 13:02:53 +0600

    use built in types in Typescript

---
+++ built-in-type/index.js
@@ -0,0 +1,9 @@
+var userID = 123;
+var firstName;
+var lastName;
+var age;
+firstName = "Mohaimin";
+lastName = "Islam";
+age = 20;
+console.log("User Id :".concat(userID, " | First Name: ").concat(firstName, " | Last Name: ").concat(lastName, " | Age: ").concat(age));
+console.log(firstName + " " + lastName);
---
+++ built-in-type/index.ts
@@ -0,0 +1,14 @@
+let userID: number = 123;
+let firstName: string;
+let lastName: string;
+let age: number;
+
+firstName = "Mohaimin";
+lastName = "Islam";
+age = 20;
+
+console.log(
+  `User Id :${userID} | First Name: ${firstName} | Last Name: ${lastName} | Age: ${age}`
+);
+
+console.log(firstName + " " + lastName);
